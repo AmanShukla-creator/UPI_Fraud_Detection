4062ad772e5a10fa6af91265a650511c026ac5ed
Prakhar Pandey <prakharpandeysar71@gmail.com>
2026-03-05 13:14:12 +0530

    Added fraud probability output

--- ml/predict.py
+++ ml/predict.py
@@ -6,12 +6,16 @@ model = pickle.load(open("ml/fraud_model.pkl", "rb"))
 
 def predict_transaction(data):
     data = np.array(data).reshape(1, -1)
-    prediction = model.predict(data)
+    prediction = model.predict(data)[0]
+    probability = model.predict_proba(data)[0][1]
 
-    if prediction[0] == 1:
-        return "Fraud"
-    else:
-        return "Not Fraud"
+    return {
+        "prediction": "Fraud" if prediction == 1 else "Not Fraud",
+        "fraud_probability": float(probability)
+    }
+
+
+    
 if __name__ == "__main__":
     sample = [0]*30
     print(predict_transaction(sample))
